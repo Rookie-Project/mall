3ef0c3a3ea1721939aee60c16a2d09eb95a936b7
ganxinming <@ganBadribbit127>
2021-12-22 17:14:31 +0800

    feat: init

---
+++ src/main/java/com/rookie/mall/Main.java
@@ -0,0 +1,12 @@
+package com.rookie.mall;
+
+/**
+ * @author ganxinming
+ * @createDate 2021/12/22
+ * @description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
