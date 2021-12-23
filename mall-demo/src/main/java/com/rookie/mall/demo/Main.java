88f4786db518da47f749207389c38caa7d8b7517
ganxinming <@ganbadribbit127>
2021-12-23 09:56:35 +0800

    feat:init 测试

--- mall-demo/src/main/java/com/rookie/mall/demo/Main.java
+++ mall-demo/src/main/java/com/rookie/mall/demo/Main.java
@@ -1,12 +1,12 @@
-package com.rookie.mall;
+package com.rookie.mall.demo;
 
 /**
  * @author ganxinming
- * @createDate 2021/12/22
+ * @createDate 2021/12/23
  * @description
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!");
+        System.out.println("mall demo");
     }
 }
